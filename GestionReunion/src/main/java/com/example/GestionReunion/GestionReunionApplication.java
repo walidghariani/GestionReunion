eacc1a9850b9e6c40ca1724383e55a7b684aa26c
WalidGhariani <WalidGhariani@WalidGhariani>
2018-07-20 16:17:43 +0100

    Project init

---
+++ GestionReunion/src/main/java/com/example/GestionReunion/GestionReunionApplication.java
@@ -0,0 +1,12 @@
+package com.example.GestionReunion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionReunionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionReunionApplication.class, args);
+	}
+}
